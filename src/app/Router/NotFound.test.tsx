4a3799f4e2356d5bd2c6415ccc54471b18a9d069
Zx55 <czr.cn.525@gmail.com>
2020-09-05 21:42:47 +0800

    fix NotFound.test.tsx

--- src/app/Router/NotFound.test.tsx
+++ src/app/Router/NotFound.test.tsx
@@ -11,11 +11,11 @@ describe('<NotFound />', () => {
     it('should have an icon, a title and a "back home" button.', () => {
         expect(wrapper.find('#not-found-icon').length).toEqual(1);
         expect(wrapper.find('#not-found-title').text()).toEqual('Sorry, the page you visited does not exist.');
-        expect(wrapper.find('#not-found-back-home-btn').first().text()).toEqual('Back home');
+        expect(wrapper.find('button#not-found-back-home-btn').text()).toEqual('Back home');
     });
 
     it('should jump to homepage when click "back home" button.', () => {
-        const btn = wrapper.find('#not-found-back-home-btn').first();
+        const btn = wrapper.find('button#not-found-back-home-btn');
         expect(wrapper.history.location.pathname).toEqual('/not-found');
         btn.simulate('click');
         expect(wrapper.history.location.pathname).toEqual('/');
